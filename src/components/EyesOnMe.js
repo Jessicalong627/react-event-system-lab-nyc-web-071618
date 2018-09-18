02e6e6391b17c759dca1f1f2ab2094963f42ec59
Jessicalong627 <jessicalong627@gmail.com>
2018-09-18 11:13:19 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -7,7 +7,7 @@ class EyesOnMe extends Component {
 
   render() {
     return (
-      <button onFocus={(e) => console.log('Good!')} onBlur={(e) => console.log('Hey! Eyes on me!')}>  </button>
+      <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')}>  </button>
     )
   }
 }
